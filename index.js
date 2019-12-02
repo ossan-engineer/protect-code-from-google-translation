44179651c8d2f74bd3293e310503c9a95a5669c1
Kiichi Tachibana <bodega1123@gmail.com>
2019-12-02 12:55:17 +0900

    Add iframe to mergedNodes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const preDomNodes = document.querySelectorAll("pre") || [];
-const figureDomNodes = document.querySelectorAll("figure") || [];
-const mergedNodes = [...preDomNodes, ...figureDomNodes];
+const preDomNodes = document.querySelectorAll('pre') || [];
+const figureDomNodes = document.querySelectorAll('figure') || [];
+const iframeDomNodes = document.querySelectorAll('iframe') || [];
+const mergedNodes = [...preDomNodes, ...figureDomNodes, ...iframeDomNodes];
 
-mergedNodes.forEach(mergedNode => mergedNode.classList.add("notranslate"));
+mergedNodes.forEach(mergedNode => mergedNode.classList.add('notranslate'));
