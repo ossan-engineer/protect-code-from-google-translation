dc92a9bf02a6877ed7d8a895a65dbc3cef2f1aef
Kiichi Tachibana <bodega1123@gmail.com>
2020-07-28 15:25:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const preDomNodes = document.querySelectorAll('pre') || [];
 const figureDomNodes = document.querySelectorAll('figure') || [];
 const iframeDomNodes = document.querySelectorAll('iframe') || [];
+const prismDomNodes = document.querySelectorAll('.prism-code') || [];
 const mergedNodes = [...preDomNodes, ...figureDomNodes, ...iframeDomNodes];
 
 mergedNodes.forEach(mergedNode => mergedNode.classList.add('notranslate'));
